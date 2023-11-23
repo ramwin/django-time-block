73218c7f0a9977098bf0a61cf3f5fb4bf90c57b7
Xiang Wang <ramwin@qq.com>
2023-11-23 21:48:22 +0800

    feat: add migrations

--- example/django_time_block/models.py
+++ example/django_time_block/models.py
@@ -11,6 +11,6 @@ class TimeBlock(models.Model):
     """
     a time block contains a duration [start_datetime: end_datetime) (not include the end_datetime)
     """
-    object_id = models.TextField(index=True)
+    object_id = models.TextField(db_index=True)
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()
